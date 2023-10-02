5b82c61030c4b649b14bf0afafaa3b9060b6d994
Damian <damian@danielcastelao.org>
2023-10-02 09:52:48 +0200

    modificado ejercicio 1!

--- Boletin01/Boletin01_01/src/Main.java
+++ Boletin01/Boletin01_01/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         // Press Alt+Intro with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Ejercicio 1 hecho!!!");
+        System.out.printf("Modificacion");
 
         // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
